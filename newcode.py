30075f776f0acb24d2c06a339c4062358d04cdb3
amarsurya101 <amarsuryavanshi5@gmail.com>
2018-02-06 13:40:47 +0530

    first file

---
+++ newcode.py
@@ -0,0 +1,15 @@
+import numpy as np
+import scipy as sp
+import matplotlib as mlt
+
+f = open("ch00_B0833-45_20150612_191438_010_1")
+g = open("gg", "w")
+
+#g.write(f.read(60))
+#for i in g:
+#	print(i)
+x = f.read()
+for line in x:
+	print(line)
+           
+
